a23a1dbc26ff185e1c3bff5df7a2b5303a3dc54b
Kanishk Srivastava <66626758+Kanishk-Srivastava@users.noreply.github.com>
2024-07-13 23:37:52 +0530

    Time: 3 ms (80.10%) | Memory: 16.7 MB (6.74%) - LeetSync

---
+++ 81-search-in-rotated-sorted-array-ii/search-in-rotated-sorted-array-ii.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    bool search(vector<int>& nums, int target) {
+        int left = 0, right = nums.size() - 1; 
+
+        while(left <= right){
+            int mid = left + (right - left)/2; 
+            if(nums[mid] == target){
+                return true;
+            }
+            if(nums[left] == nums[mid] && nums[mid] == nums[right]){
+                left++; 
+                right--; 
+                continue; 
+            }
+            //check if the left part is sorted
+            if(nums[left] <= nums[mid]){
+               //check if the target lies between this sorted part 
+               if(target>= nums[left] && target <= nums[mid]){
+                    right = mid - 1; //do binary search on this part
+               } else {
+                    left = mid + 1; //discard this part of the array as target doesnt lie here
+               }
+            } else { //left is not sorted, right is sorted
+                if(target >= nums[mid] && target <= nums[right]){
+                    left = mid + 1; 
+                } else {
+                    right = mid - 1; 
+                }
+            }
+        }
+        return false; 
+    }
+};
