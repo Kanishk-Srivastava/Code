65da6ddbd41dc78337d87fd5deff675d6d0b9569
Kanishk Srivastava <66626758+Kanishk-Srivastava@users.noreply.github.com>
2025-01-18 01:29:38 +0530

    Time: 0 ms (100%), Space: 14 MB (70.56%) - LeetHub

--- 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.cpp
+++ 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.cpp
@@ -1,31 +1,25 @@
 class Solution {
 public:
     int findMin(vector<int>& nums) {
-        //approach -> keep min from the sorted part and discard that part as we have already got the min from that, then keep eliminating the search space
-        int left=0, right = nums.size() - 1; 
-        int minElement = INT_MAX; //find min using min()
-        while(left <= right){
-            int mid = left + (right-left)/2; 
+        int ans = INT_MAX; 
+        int left = 0, right = nums.size() - 1; 
 
+        while(left <= right){    
+        int mid = left + (right-left)/2;
 
-
-            //if the whole array is sorted, return the first element => nums[left]
-            if(nums[left] <= nums[right]){
-                minElement = min(minElement, nums[left]); 
-                break; 
-            }
-
-            //if left part is sorted 
-            if(nums[left] <= nums[mid]){
-                //keep the minimum and discard this part of the array
-                minElement = min(minElement, nums[left]); 
-                left = mid + 1; 
+        if(nums[left] <= nums[right])
+        {
+            ans = min(ans, nums[left]); 
+            break; 
+        }
+        if(nums[left]<=nums[mid]){
+                ans =min(ans, nums[left]);
+                left = mid + 1;
             } else {
-                //keep the minimum and discard this part
-                minElement = min(minElement, nums[mid]); 
+                ans = min(ans, nums[mid]);
                 right = mid - 1; 
             }
         }
-        return minElement; 
+        return ans; 
     }
 };
