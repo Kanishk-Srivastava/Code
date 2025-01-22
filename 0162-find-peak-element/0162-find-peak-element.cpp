36cb83df5364f99d0a637379dad3178b6536676f
Kanishk Srivastava <66626758+Kanishk-Srivastava@users.noreply.github.com>
2025-01-22 19:35:52 +0530

    Time: 0 ms (100%), Space: 12.6 MB (27.31%) - LeetHub

--- 0162-find-peak-element/0162-find-peak-element.cpp
+++ 0162-find-peak-element/0162-find-peak-element.cpp
@@ -1,22 +1,31 @@
 class Solution {
 public:
     int findPeakElement(vector<int>& nums) {
-        int left = 1, right = nums.size() - 2; 
-        if(nums.size() == 1) return 0; 
-        if(nums[0] > nums[1]) return nums[0]; 
-        if(nums[nums.size() - 1] > nums[nums.size() - 2]) return nums[nums.size() - 1]; 
+        int ans=0, left = 1, right = nums.size() - 2;
+
+        if(nums.size() == 1){
+            return 0; 
+        }
+        if(nums[0] > nums[1]){
+            return 0; 
+        }
+        if(nums[nums.size() - 1] > nums[nums.size() - 2]){
+            return nums.size() - 1; 
+        }
 
         while(left <= right){
-            int mid = left + (right - left) / 2; 
+            int mid = left + (right - left)/2; 
 
-            if(nums[mid] > nums[mid-1] && nums[mid] > nums[mid+1]){
-                return mid;         
-            } else if(nums[mid] > nums[mid-1]) {
+            if(nums[mid-1]<nums[mid] && nums[mid]>nums[mid+1]){
+                return mid; //this is the peak 
+            } else if(nums[mid] > nums[mid - 1]){
+                //peak exists in the right part eliminate sorted part
                 left = mid + 1; 
             } else {
                 right = mid - 1; 
-            }
+            }            
         }
-        return -1; 
+        return -1;
+        
     }
 };
