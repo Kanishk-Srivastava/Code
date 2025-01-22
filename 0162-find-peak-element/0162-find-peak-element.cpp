f668600f805e0163afc624c8ab6f5c93327929f2
Kanishk Srivastava <66626758+Kanishk-Srivastava@users.noreply.github.com>
2025-01-22 19:35:26 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0162-find-peak-element/0162-find-peak-element.cpp
+++ 0162-find-peak-element/0162-find-peak-element.cpp
@@ -4,14 +4,14 @@ public:
         int left = 1, right = nums.size() - 2; 
         if(nums.size() == 1) return 0; 
         if(nums[0] > nums[1]) return nums[0]; 
-        if(nums[nums.size() - 2] > nums[nums.size() - 1]) return 0; 
+        if(nums[nums.size() - 1] > nums[nums.size() - 2]) return nums[nums.size() - 1]; 
 
         while(left <= right){
             int mid = left + (right - left) / 2; 
 
-            if(nums[mid] > nums[mid-1] && nums[mid] > nums[mid+1]) return mid; 
-
-            if(nums[mid] > nums[mid-1]) {
+            if(nums[mid] > nums[mid-1] && nums[mid] > nums[mid+1]){
+                return mid;         
+            } else if(nums[mid] > nums[mid-1]) {
                 left = mid + 1; 
             } else {
                 right = mid - 1; 
