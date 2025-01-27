f7d45da1e9686a6c6839eca7c8d2d57ef9bf9aeb
Kanishk Srivastava <66626758+Kanishk-Srivastava@users.noreply.github.com>
2025-01-27 22:47:19 +0530

    Time: 0 ms (100%), Space: 10.4 MB (82.54%) - LeetHub

--- 0410-split-array-largest-sum/0410-split-array-largest-sum.cpp
+++ 0410-split-array-largest-sum/0410-split-array-largest-sum.cpp
@@ -1,38 +1,38 @@
 class Solution {
 public:
-
-int findStudent(vector<int>& arr, int pages){
-    int student = 1, pagesStudent = 0; 
-    for(int i=0; i<arr.size(); i++){
-        if(pagesStudent + arr[i] <= pages){
-            pagesStudent += arr[i]; 
-        } else {
-            student++; 
-            pagesStudent = arr[i]; 
-        }
-    }
-    return student; 
-}
-
-int findPages(vector<int>& arr, int n, int m) {
-    if(arr.size() < m){
-        return -1; 
-    }
-    int left = *max_element(arr.begin(), arr.end()); 
-    int right = accumulate(arr.begin(), arr.end(), 0); 
-
-    while(left <= right){
-        int pages = left + (right - left)/2; 
-        int student = findStudent(arr, pages); 
-        if(student <= m){
-            right = pages - 1; 
-        } else {
-            left = pages + 1; 
-        }
-    }
-    return left; 
-}
     int splitArray(vector<int>& nums, int k) {
-        return findPages(nums, nums.size(), k);
+        if (nums.size() < k) {
+            return -1;
+        }
+
+        int left = *max_element(nums.begin(), nums.end());
+        int right = accumulate(nums.begin(), nums.end(), 0);
+
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+
+            int number = findSums(nums, mid);
+
+            if (number <= k) {
+                right = mid - 1;
+            } else
+                left = mid + 1;
+        }
+        return left;
+    }
+
+    int findSums(vector<int>& nums, int mid) {
+        int sum = 0;
+        int count = 1;
+
+        for (int i = 0; i < nums.size(); i++) {
+            if (nums[i] + sum <= mid) {
+                sum += nums[i];
+            } else {
+                sum = nums[i];
+                count++;
+            }
+        }
+        return count;
     }
 };
