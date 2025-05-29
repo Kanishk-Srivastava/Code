ef353da2b890e5465cff6966366cf1b58ab38343
Kanishk Srivastava <66626758+Kanishk-Srivastava@users.noreply.github.com>
2025-05-29 23:48:49 +0530

    Time: 0 ms (100%), Space: 11.2 MB (45.53%) - LeetHub

--- 1878-check-if-array-is-sorted-and-rotated/1878-check-if-array-is-sorted-and-rotated.cpp
+++ 1878-check-if-array-is-sorted-and-rotated/1878-check-if-array-is-sorted-and-rotated.cpp
@@ -2,20 +2,16 @@ class Solution {
 public:
     bool check(vector<int>& nums) {
         int n = nums.size(); 
-        int count=0; 
+        int count = 0; 
 
-        //check if there exist any point of rotation
-        for(int i=1; i<n; i++){
-            if(nums[i-1] > nums[i]){
-                count++;
+        for(int i=0; i<n; i++){
+            if(nums[i] > nums[(i+1) % n]){
+                count++; 
             }
         }
 
-        //check for the last and first element (condition true then it is sorted)
-        if(nums[n-1] > nums[0]){
-            count++; 
-        }
-
-        return count<=1; 
+        if(count == 0 || count == 1){
+            return true; 
+        } else return false; 
     }
 };
