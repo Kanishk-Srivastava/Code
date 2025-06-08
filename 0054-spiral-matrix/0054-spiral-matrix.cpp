1a991765ac342f826e9d35aa9f6aef5c635a130b
Kanishk Srivastava <66626758+Kanishk-Srivastava@users.noreply.github.com>
2025-06-08 23:07:32 +0530

    Time: 0 ms (100%), Space: 9.6 MB (13.94%) - LeetHub

--- 0054-spiral-matrix/0054-spiral-matrix.cpp
+++ 0054-spiral-matrix/0054-spiral-matrix.cpp
@@ -1,41 +1,43 @@
 class Solution {
 public:
+    vector<int> result;
+
+    void spiralHelper(vector<vector<int>>& matrix, int top, int bottom, int left, int right) {
+        // Base case: if bounds are invalid
+        if (top > bottom || left > right) return;
+
+        // Top row
+        for (int j = left; j <= right; j++)
+            result.push_back(matrix[top][j]);
+        top++;
+
+        // Right column
+        for (int i = top; i <= bottom; i++)
+            result.push_back(matrix[i][right]);
+        right--;
+
+        // Bottom row (if any)
+        if (top <= bottom) {
+            for (int j = right; j >= left; j--)
+                result.push_back(matrix[bottom][j]);
+            bottom--;
+        }
+
+        // Left column (if any)
+        if (left <= right) {
+            for (int i = bottom; i >= top; i--)
+                result.push_back(matrix[i][left]);
+            left++;
+        }
+
+        // Recurse on the inner submatrix
+        spiralHelper(matrix, top, bottom, left, right);
+    }
+
     vector<int> spiralOrder(vector<vector<int>>& matrix) {
         int n = matrix.size();
         int m = matrix[0].size();
-
-        int top = 0, down = n - 1, left = 0, right = m - 1;
-        int dir = 0;
-        vector<int> result;
-        while (top <= down && left <= right) {
-            if (dir == 0) {
-                for (int i = left; i <= right; i++) {
-                    result.push_back(matrix[top][i]);
-                }
-                top++;
-            }
-             else if (dir == 1) {
-                for (int i = top; i <= down; i++) {
-                    result.push_back(matrix[i][right]);
-                }
-                right--;
-            }
-            else if (dir == 2) {
-                for (int i = right; i >= left; i--) {
-                    result.push_back(matrix[down][i]);
-                }
-                down--;
-            }
-            else if (dir == 3) {
-                for (int i = down; i >= top; i--) {
-                    result.push_back(matrix[i][left]);
-                }
-                left++;
-            }
-            dir++;
-            if (dir == 4)
-                dir = 0;
-        }
-        return result; 
+        spiralHelper(matrix, 0, n - 1, 0, m - 1);
+        return result;
     }
 };
