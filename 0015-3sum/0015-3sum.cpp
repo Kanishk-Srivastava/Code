ca1fb5a1dff1163f996562535abc62efcf9553df
Kanishk Srivastava <66626758+Kanishk-Srivastava@users.noreply.github.com>
2025-06-16 19:44:25 +0530

    Time: 40 ms (91.75%), Space: 33 MB (23.2%) - LeetHub

--- 0015-3sum/0015-3sum.cpp
+++ 0015-3sum/0015-3sum.cpp
@@ -1,30 +1,38 @@
 class Solution {
 public:
-    vector<vector<int>> threeSum(vector<int>& nums) {
-        vector<vector<int>> ans; 
-        sort(nums.begin(), nums.end()); 
-        for(int i=0; i<nums.size(); i++){
-            if(i>0 && nums[i] == nums[i-1]) continue; 
-
-            int j = i+1; 
-            int k = nums.size() - 1; 
-
-            while(j<k){
-                int sum = nums[i] + nums[j] + nums[k]; 
-
-                if(sum > 0){
+    vector<vector<int>> ans;
+    void twoSum(int target, vector<int>& nums, int j, int k) {
+        while (j < k) {
+            if (nums[j] + nums[k] > target)
+                k--;
+            else if (nums[j] + nums[k] < target)
+                j++;
+            else {
+                while (j < k && nums[j] == nums[j + 1])
+                    j++;
+                while (j < k && nums[k] == nums[k - 1])
                     k--;
-                } else if(sum < 0){
-                    j++; 
-                } else {
-                    ans.push_back({nums[i], nums[j], nums[k]}); 
-                    j++; 
-                    k--; 
-                    while(j < k && nums[j] == nums[j-1]) j++; 
-                    while(j < k && nums[k] == nums[k+1]) k--; 
-                }
+
+                ans.push_back({-target, nums[j], nums[k]});
+                j++; 
+                k--; 
             }
         }
-        return ans; 
+    }
+
+    vector<vector<int>> threeSum(vector<int>& nums) {
+        vector<vector<int>> result;
+        if (nums.size() < 3)
+            return {};
+
+        sort(nums.begin(), nums.end());
+
+        for (int i = 0; i < nums.size(); i++) {
+            if (i > 0 && nums[i] == nums[i - 1])
+                continue;
+            int target = -nums[i];
+            twoSum(target, nums, i + 1, nums.size() - 1);
+        }
+        return ans;
     }
 };
