b490cdb4387894dd08cb911813ff6c284069f463
Kanishk Srivastava <66626758+Kanishk-Srivastava@users.noreply.github.com>
2025-07-23 23:44:26 +0530

    Time: 0 ms (100%), Space: 15.7 MB (32.91%) - LeetHub

--- 0031-next-permutation/0031-next-permutation.cpp
+++ 0031-next-permutation/0031-next-permutation.cpp
@@ -1,25 +1,32 @@
 class Solution {
 public:
     void nextPermutation(vector<int>& nums) {
-        int idx = -1; 
-
-        for(int i=nums.size() - 2; i>=0; i--){
-            if(nums[i]<nums[i+1]){
-                idx = i; 
-                break; 
+        int pivotIdx = -1;
+        int swapIdx = -1;
+        int n = nums.size();
+        // find pivot index
+        for (int i = n - 2; i >= 0; i--) {
+            if (nums[i] < nums[i + 1]) {
+                pivotIdx = i;
+                break;
             }
         }
-        if(idx == -1){
-            reverse(nums.begin(), nums.end()); 
-            return; 
+
+
+        if (pivotIdx == -1) {
+            reverse(nums.begin(), nums.end());
+            return;
         }
 
-        for(int i = nums.size() - 1; i>idx; i--){
-            if(nums[i]>nums[idx]){
-                swap(nums[i], nums[idx]); 
-                break; 
+        // 1 5 4 3 2 -> 2 5 4 3 1
+        // find swap index
+        for (int i = n - 1; i >= 0; i--) {
+            if (nums[i] > nums[pivotIdx]) {
+                swapIdx = i;
+                break;
             }
         }
-        reverse(nums.begin() + idx + 1, nums.end()); 
+        swap(nums[pivotIdx], nums[swapIdx]);
+        reverse(nums.begin() + pivotIdx + 1, nums.end());
     }
 };
