6ec8a4f6c44d415afd5a09b4b9d6721e22534ff0
Kanishk Srivastava <66626758+Kanishk-Srivastava@users.noreply.github.com>
2025-07-23 23:57:17 +0530

    Time: 0 ms (100%), Space: 18.5 MB (95.72%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
@@ -1,53 +1,44 @@
 class Solution {
 public:
     void setZeroes(vector<vector<int>>& matrix) {
-        bool firstRowHasZero = false;
-        bool firstColHasZero = false;
+        bool firstRowHasZero = false, firstColHasZero = false; 
 
-        // Check if first column has zero
-        for (int i = 0; i < matrix.size(); i++) {
-            if (matrix[i][0] == 0) {
-                firstColHasZero = true;
-            }
+        //check if first row has 0
+        for(int i=0; i<matrix.size(); i++){
+            if(matrix[i][0] == 0) firstRowHasZero = true; 
         }
 
-        // Check if first row has zero
-        for (int j = 0; j < matrix[0].size(); j++) {
-            if (matrix[0][j] == 0) {
-                firstRowHasZero = true;
-            }
+        //check if first col has 0
+        for(int i=0; i<matrix[0].size(); i++){
+            if(matrix[0][i] == 0) firstColHasZero = true; 
         }
 
-        // Traverse internal rows cols 
-        for (int i = 1; i < matrix.size(); i++) {
-            for (int j = 1; j < matrix[0].size(); j++) {
-                if (matrix[i][j] == 0) {
-                    matrix[i][0] = 0;
-                    matrix[0][j] = 0;
+        //use 1st row & col as markers and traverse internal matrix
+        for(int i=1; i<matrix.size(); i++){
+            for(int j=1; j<matrix[0].size(); j++){
+                if(matrix[i][j] == 0){
+                    matrix[0][j] = 0; 
+                    matrix[i][0] = 0; 
                 }
             }
         }
 
-        // Apply zeroes based on markers
-        for (int i = 1; i < matrix.size(); i++) {
-            for (int j = 1; j < matrix[0].size(); j++) {
-                if (matrix[i][0] == 0 || matrix[0][j] == 0) {
-                    matrix[i][j] = 0;
+        //fill the marked row col as 0
+        for(int i=1; i<matrix.size(); i++){
+            for(int j=1; j<matrix[0].size(); j++){
+                if(matrix[0][j] == 0 || matrix[i][0] == 0){
+                    matrix[i][j] = 0; 
                 }
             }
         }
 
-        // Final row and column zeroing
-        if (firstRowHasZero) {
-            for (int j = 0; j < matrix[0].size(); j++) {
-                matrix[0][j] = 0;
-            }
+        //now if firstRow or firstCol has zero then fill them to 0
+        for(int i=0; i<matrix.size(); i++){
+           if(firstRowHasZero) matrix[i][0] = 0; 
+        }
+        for(int i=0; i<matrix[0].size(); i++){
+           if(firstColHasZero) matrix[0][i] = 0; 
         }
 
-        if (firstColHasZero) {
-            for (int i = 0; i < matrix.size(); i++) {
-                matrix[i][0] = 0;
-            }
-        }
     }
 };
