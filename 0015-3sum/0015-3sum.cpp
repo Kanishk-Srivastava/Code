7097a848a9055aed288650107460649df73f7cb5
Kanishk Srivastava <66626758+Kanishk-Srivastava@users.noreply.github.com>
2025-07-26 01:10:29 +0530

    Time: 57 ms (29.84%), Space: 35.7 MB (16.13%) - LeetHub

--- 0015-3sum/0015-3sum.cpp
+++ 0015-3sum/0015-3sum.cpp
@@ -1,38 +1,33 @@
 class Solution {
 public:
-    vector<vector<int>> ans;
-    void twoSum(int target, vector<int>& nums, int j, int k) {
-        while (j < k) {
-            if (nums[j] + nums[k] > target)
-                k--;
-            else if (nums[j] + nums[k] < target)
-                j++;
+    vector<vector<int>> twoSum(int target, vector<int>& nums, int j, int k){
+        vector<vector<int>> result; 
+        while(j < k){
+            if(nums[j] + nums[k] > target) k--; 
+            else if(nums[j] + nums[k] < target) j++; 
             else {
-                while (j < k && nums[j] == nums[j + 1])
-                    j++;
-                while (j < k && nums[k] == nums[k - 1])
-                    k--;
-
-                ans.push_back({-target, nums[j], nums[k]});
+                result.push_back({-target, nums[j], nums[k]}); 
                 j++; 
                 k--; 
+                while(j < k && nums[j] == nums[j-1]) j++; 
+                while(j < k && nums[k] == nums[k+1]) k--; 
             }
         }
+        return result; 
     }
 
     vector<vector<int>> threeSum(vector<int>& nums) {
-        vector<vector<int>> result;
-        if (nums.size() < 3)
-            return {};
+        vector<vector<int>> result; 
+        if(nums.size() < 3) return {}; 
 
-        sort(nums.begin(), nums.end());
+        sort(nums.begin(), nums.end()); 
 
-        for (int i = 0; i < nums.size(); i++) {
-            if (i > 0 && nums[i] == nums[i - 1])
-                continue;
-            int target = -nums[i];
-            twoSum(target, nums, i + 1, nums.size() - 1);
+        for(int i=0; i<nums.size(); i++){
+            if(i > 0 && nums[i] == nums[i-1]) continue; 
+            int target = -nums[i]; 
+            vector<vector<int>>pairs = twoSum(target, nums, i+1, nums.size() - 1); 
+            result.insert(result.end(), pairs.begin(), pairs.end()); 
         }
-        return ans;
+        return result; 
     }
 };
