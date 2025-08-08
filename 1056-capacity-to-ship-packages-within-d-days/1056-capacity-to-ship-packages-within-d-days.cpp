46ac0997e3871d37964991595275f6e4007a8f2c
Kanishk Srivastava <66626758+Kanishk-Srivastava@users.noreply.github.com>
2025-08-08 23:57:11 +0530

    Time: 24 ms (5.51%), Space: 35 MB (69.1%) - LeetHub

--- 1056-capacity-to-ship-packages-within-d-days/1056-capacity-to-ship-packages-within-d-days.cpp
+++ 1056-capacity-to-ship-packages-within-d-days/1056-capacity-to-ship-packages-within-d-days.cpp
@@ -1,32 +1,29 @@
 class Solution {
 public:
     int shipWithinDays(vector<int>& weights, int days) {
-        int left = *max_element(weights.begin(), weights.end()); 
-        int right = accumulate(weights.begin(), weights.end(), 0); 
-
-        while(left <= right){
-            int mid = left + (right- left)/2; 
-            int numberOfDays = findDays(weights, mid); 
-            if(numberOfDays <= days){
-                right = mid - 1;
-            } else {
-                left = mid + 1; 
-            }
+        int low = *max_element(weights.begin(), weights.end()); 
+        int high = accumulate(weights.begin(), weights.end(), 0); 
+        int ans = -1; 
+        while(low <= high){
+            int mid= low + (high - low) / 2; 
+            if(canShip(weights, days, mid)){
+                ans = mid; 
+                high = mid - 1; 
+            } else low = mid + 1; 
         }
-        return left; 
+        return ans; 
     }
 
-    int findDays(vector<int>& weights, int mid){
-        int day = 1; 
-        int weight = 0; 
+    bool canShip(vector<int>& weights, int days, int mid){
+        int daysTaken = 1; 
+        int currentWeight = 0; 
         for(int i=0; i<weights.size(); i++){
-            if(weight + weights[i] <= mid){
-              weight += weights[i];
-            } else {
-                weight = weights[i];
-                day++; 
+            if(currentWeight + weights[i] > mid){
+                daysTaken++; 
+                currentWeight = 0; 
             }
+            currentWeight += weights[i]; 
         }
-        return day; 
+        return daysTaken <= days; 
     }
 };
