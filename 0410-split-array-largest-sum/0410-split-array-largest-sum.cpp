bbd80d462e0a0d05c21365172c900517c2a946fe
Kanishk Srivastava <66626758+Kanishk-Srivastava@users.noreply.github.com>
2025-08-10 01:29:46 +0530

    Time: 0 ms (100%), Space: 10.4 MB (86.72%) - LeetHub

--- 0410-split-array-largest-sum/0410-split-array-largest-sum.cpp
+++ 0410-split-array-largest-sum/0410-split-array-largest-sum.cpp
@@ -1,38 +1,27 @@
 class Solution {
 public:
     int splitArray(vector<int>& nums, int k) {
-        if (nums.size() < k) {
-            return -1;
-        }
-
-        int left = *max_element(nums.begin(), nums.end());
-        int right = accumulate(nums.begin(), nums.end(), 0);
-
-        while (left <= right) {
-            int mid = left + (right - left) / 2;
-
-            int number = findSums(nums, mid);
-
-            if (number <= k) {
-                right = mid - 1;
-            } else
-                left = mid + 1;
-        }
-        return left;
-    }
-
-    int findSums(vector<int>& nums, int mid) {
-        int sum = 0;
-        int count = 1;
-
-        for (int i = 0; i < nums.size(); i++) {
-            if (nums[i] + sum <= mid) {
-                sum += nums[i];
-            } else {
-                sum = nums[i];
-                count++;
+        int low = *max_element(nums.begin(), nums.end()); 
+        int high = accumulate(nums.begin(), nums.end(), 0); 
+        int ans = -1; 
+        while(low <= high){
+            int mid = low + (high - low) / 2; 
+            int sum = 0; 
+            int splitCount = 1; 
+            for(int i=0; i<nums.size(); i++){
+                if(sum + nums[i] > mid){
+                    splitCount++; 
+                    sum = 0; 
+                }
+                sum += nums[i]; 
             }
+
+            if(splitCount <= k){
+                ans = mid; 
+                high = mid - 1;
+            } else 
+                low = mid + 1; 
         }
-        return count;
+        return ans; 
     }
 };
