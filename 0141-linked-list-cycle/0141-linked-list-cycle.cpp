5d229c47501d3f3603b77067d43070929478ec68
Kanishk Srivastava <66626758+Kanishk-Srivastava@users.noreply.github.com>
2025-08-31 00:44:15 +0530

    Time: 4 ms (97.16%), Space: 11.9 MB (23.24%) - LeetHub

--- 0141-linked-list-cycle/0141-linked-list-cycle.cpp
+++ 0141-linked-list-cycle/0141-linked-list-cycle.cpp
@@ -9,15 +9,27 @@
 class Solution {
 public:
     bool hasCycle(ListNode *head) {
-        unordered_set<ListNode*> visited; 
+        // unordered_set<ListNode*> visited; 
 
-        while(head != NULL){
-            if(visited.find(head) != visited.end()){
-                return true;
-            } else {
-                visited.insert(head); 
-                head = head->next; 
-            }
+        // while(head != NULL){
+        //     if(visited.find(head) != visited.end()){
+        //         return true;
+        //     } else {
+        //         visited.insert(head); 
+        //         head = head->next; 
+        //     }
+        // }
+        // return false; 
+//FLOYD CYCLE DETECTION
+        if(!head || !head->next) return false; 
+
+        ListNode* slow = head; 
+        ListNode* fast = head; 
+
+        while(fast && fast->next){
+            slow = slow->next; 
+            fast = fast -> next -> next; 
+            if(slow == fast) return true; 
         }
         return false; 
     }
