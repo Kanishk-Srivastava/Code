ae3ad3630c80ba853bd0f5d063a7dec3bbfc850d
Kanishk Srivastava <66626758+Kanishk-Srivastava@users.noreply.github.com>
2025-09-15 01:15:39 +0530

    Time: 3 ms (91.63%), Space: 15 MB (68.82%) - LeetHub

--- 0138-copy-list-with-random-pointer/0138-copy-list-with-random-pointer.cpp
+++ 0138-copy-list-with-random-pointer/0138-copy-list-with-random-pointer.cpp
@@ -5,7 +5,7 @@ public:
     int val;
     Node* next;
     Node* random;
-    
+
     Node(int _val) {
         val = _val;
         next = NULL;
@@ -17,24 +17,61 @@ public:
 class Solution {
 public:
     Node* copyRandomList(Node* head) {
-        if(!head) return nullptr; 
-        unordered_map <Node*, Node*> nodeMap; 
-        nodeMap[nullptr] = nullptr; 
+        if (!head)
+            return nullptr;
         Node* curr = head;
-        while(curr){
-            Node* newNode = new Node(curr->val); 
-            nodeMap[curr] = newNode; 
-            curr = curr->next; 
+
+        while (curr) {
+            Node* newNode = new Node(curr->val);
+            newNode->next = curr->next;
+            curr->next = newNode;
+            curr = curr->next->next;
         }
 
-        curr = head; 
+        curr = head;
 
-        while(curr){
-            nodeMap[curr]->next = nodeMap[curr->next]; 
-            nodeMap[curr]->random = nodeMap[curr->random]; 
-            curr = curr->next; 
+        while (curr) {
+            curr->next->random = (curr->random)
+                                     ? (curr->random->next)
+                                     : nullptr;
+         curr = curr->next->next;
         }
 
-        return nodeMap[head]; 
+        Node* newHead = head->next;
+
+        curr = head;
+
+        while (curr) {
+            Node* clone = curr->next;
+            curr->next = clone->next;
+            clone->next = (clone->next) ? clone->next->next : nullptr;
+            curr = curr->next;
+        }
+        return newHead;
     }
 };
+
+// class Solution {
+// public:
+//     Node* copyRandomList(Node* head) {
+//         if(!head) return nullptr;
+//         unordered_map <Node*, Node*> nodeMap;
+//         nodeMap[nullptr] = nullptr;
+//         Node* curr = head;
+//         while(curr){
+//             Node* newNode = new Node(curr->val);
+//             nodeMap[curr] = newNode;
+//             curr = curr->next;
+//         }
+
+//         curr = head;
+
+//         while(curr){
+//             nodeMap[curr]->next = nodeMap[curr->next];
+//             nodeMap[curr]->random = nodeMap[curr->random];
+//             curr = curr->next;
+//         }
+
+//         return nodeMap[head];
+//     }
+// };
