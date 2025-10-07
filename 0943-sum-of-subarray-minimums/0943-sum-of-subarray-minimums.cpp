6521fc0f72488c9db4b78a4ae2d8d7f94cf4f89e
Kanishk Srivastava <66626758+Kanishk-Srivastava@users.noreply.github.com>
2025-10-07 23:26:31 +0530

    Time: 15 ms (70%), Space: 47.1 MB (62.69%) - LeetHub

--- 0943-sum-of-subarray-minimums/0943-sum-of-subarray-minimums.cpp
+++ 0943-sum-of-subarray-minimums/0943-sum-of-subarray-minimums.cpp
@@ -4,29 +4,31 @@ class Solution {
 public:
     int sumSubarrayMins(vector<int>& arr) {
         int n = arr.size(); 
-        vector<int> left(n), right(n); 
-        stack<int> st1, st2; 
-
+        vector<int> PSE(n), NSE(n); 
+        stack<int> st1,st2;  
         for(int i=0; i<n; i++){
-            while(!st1.empty() && arr[st1.top()] > arr[i]){
+            while(!st1.empty() && arr[st1.top()] >= arr[i]){
                 st1.pop(); 
             }
-            left[i] = st1.empty() ? (i+1) : i - st1.top(); 
+            PSE[i] = st1.empty() ? i + 1 : i - st1.top(); 
             st1.push(i); 
         }
 
-        for(int i= n - 1; i>= 0; i--){
-            while(!st2.empty() && arr[st2.top()] >= arr[i]){
+        for(int i = n - 1; i>= 0; i--){
+            while(!st2.empty() && arr[st2.top()] > arr[i]){
                 st2.pop(); 
             }
-            right[i] = st2.empty() ? (n-i) : st2.top() - i; 
+            NSE[i] = st2.empty() ? n - i : st2.top() - i; 
             st2.push(i); 
         }
-        long long ans = 0; 
-        for(int i = 0; i<n; i++){
-            long long contribution = (1LL * left[i] * right[i] * arr[i]) % MOD; 
-            ans = (ans + contribution) % MOD; 
+
+        long long sum = 0; 
+
+        for(int i=0; i<n; i++){
+            long long contribution = (1LL * NSE[i] * PSE[i] * arr[i]); 
+            sum = (sum + contribution) % MOD; 
         }
-        return (int) ans; 
+
+        return (int) sum; 
     }
 };
