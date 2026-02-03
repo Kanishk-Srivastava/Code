6747350badc0595b4f854a6d6813d9e68bb5fcf8
Kanishk Srivastava <66626758+Kanishk-Srivastava@users.noreply.github.com>
2026-02-04 00:37:05 +0530

    Time: 9 ms (52.38%), Space: 11.8 MB (53.2%) - LeetHub

--- 0141-linked-list-cycle/0141-linked-list-cycle.cpp
+++ 0141-linked-list-cycle/0141-linked-list-cycle.cpp
@@ -27,8 +27,8 @@ public:
         ListNode* fast = head; 
 
         while(fast && fast->next){
-            slow = slow->next; 
-            fast = fast -> next -> next; 
+            slow = slow -> next; 
+            fast = fast->next->next; 
             if(slow == fast) return true; 
         }
         return false; 
