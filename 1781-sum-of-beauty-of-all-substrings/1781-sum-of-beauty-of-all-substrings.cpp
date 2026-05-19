319568d4966fd96913491f4d3c08fcf01ba7d764
Kanishk Srivastava <66626758+Kanishk-Srivastava@users.noreply.github.com>
2026-05-20 00:57:18 +0530

    Time: 57 ms (89.03%), Space: 11.2 MB (63.72%) - LeetHub

--- 1781-sum-of-beauty-of-all-substrings/1781-sum-of-beauty-of-all-substrings.cpp
+++ 1781-sum-of-beauty-of-all-substrings/1781-sum-of-beauty-of-all-substrings.cpp
@@ -3,22 +3,23 @@ public:
     int beautySum(string s) {
         int ans = 0; 
 
+
         for(int i=0; i<s.size(); i++){
             vector<int> freq(26,0); 
+            for(int j = i; j<s.size(); j++){
+                freq[s[j] - 'a']++; 
 
-            for(int j=i; j<s.size(); j++){
-                freq[s[j] - 'a']++; //freq of each char counter array
-                int maxF = INT_MIN, minF = INT_MAX; 
+                int minF = INT_MAX, maxF = INT_MIN; 
 
                 for(auto f : freq){
                     if(f > 0){
-                        maxF = max(maxF, f); 
                         minF = min(minF, f); 
+                        maxF = max(maxF, f); 
                     }
                 }
-
-                ans += (maxF - minF); 
+            ans += (maxF - minF); 
             }
+
         }
 
         return ans; 
