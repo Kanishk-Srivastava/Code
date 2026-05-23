d7323c4fd5e86b59c6a42291ae9919d01f789745
Kanishk Srivastava <66626758+Kanishk-Srivastava@users.noreply.github.com>
2026-05-23 15:30:50 +0530

    Time: 0 ms (100%), Space: 15.2 MB (66.52%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
@@ -6,19 +6,22 @@ public:
         while(low <= high){
             int mid = low + (high - low) / 2; 
 
-            if(target == nums[mid]){
+            if(nums[mid] == target){
                 return mid; 
-            } else if(nums[low] <= nums[mid]){
-                //left part is sorted search here 
-                if(target >= nums[low] && target <= nums[mid]){
-                    high = mid-1;
+            }
+
+            if(nums[low] <= nums[mid]){
+                if(nums[low] <= target && target <= nums[mid]){
+                    high = mid - 1; 
                 } else low = mid + 1; 
             } else {
-                if(target >= nums[mid] && target <= nums[high]){
-                    low = mid + 1;
+                if(nums[mid] <= target && target <= nums[high]){
+                    low = mid + 1; 
                 } else high = mid - 1; 
             }
         }
+
         return -1; 
+
     }
 };
