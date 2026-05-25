3dee0fd6993059de0264cb6940130a9887d233f1
Kanishk Srivastava <66626758+Kanishk-Srivastava@users.noreply.github.com>
2026-05-26 00:01:21 +0530

    Time: 0 ms (100%), Space: 11.6 MB (26.53%) - LeetHub

--- 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.cpp
+++ 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.cpp
@@ -1,28 +1,26 @@
 class Solution {
 public:
-    typedef pair<char, int> P;
+    typedef pair<char,int> P; 
     string frequencySort(string s) {
         vector<P> vec(123); 
         string result; 
-        for(char &ch : s){
+
+        for(char ch : s){
             int freq = vec[ch].second; 
             vec[ch] = {ch, freq+1}; 
         }
 
         auto lambda = [&](P &p1, P &p2){
-            return p1.second > p2.second; 
-        };
+            return p1.second > p2.second;
+        }; 
 
-        sort(begin(vec), end(vec), lambda); 
-
+        sort(vec.begin(), vec.end(), lambda);
         for(int i=0; i<vec.size(); i++){
-            if(vec[i].second > 0){
-                char ch = vec[i].first; 
-                int freq = vec[i].second; 
+            int freq = vec[i].second; 
+            char ch = vec[i].first; 
 
-                string temp = string(freq, ch); 
-                result += temp; 
-            }
+            string temp = string(freq, ch); 
+            result += temp; 
         }
 
         return result; 
