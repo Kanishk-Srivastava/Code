879f7235eb36140167e9568c7823d965834233e8
Kanishk Srivastava <66626758+Kanishk-Srivastava@users.noreply.github.com>
2026-06-23 01:18:04 +0530

    Time: 0 ms (100%), Space: 9.4 MB (51.7%) - LeetHub

--- 0054-spiral-matrix/0054-spiral-matrix.cpp
+++ 0054-spiral-matrix/0054-spiral-matrix.cpp
@@ -1,48 +1,44 @@
 class Solution {
 public:
-    vector<int> result;
+    vector<int> ans; 
+    void spiralHelper(vector<vector<int>>& matrix, int top, int bottom, int left, int right){
 
-    void spiralHelper(vector<vector<int>>& matrix, int top, int bottom,
-                      int left, int right) {
-        // Base case if bounds are invalid
-        if (top > bottom || left > right)
-            return;
-
-        // Top row
-        for (int i = left; i <= right; i++) {
-            result.push_back(matrix[top][i]);
+        if(top > bottom || left > right){
+            return; 
         }
-        top++;
 
-        // Right column
-        for (int i = top; i <= bottom; i++) {
-            result.push_back(matrix[i][right]);
+        for(int i=left; i<=right; i++){
+            ans.push_back(matrix[top][i]); 
+        }
+        top++; 
+
+        for(int i=top; i<=bottom; i++){
+            ans.push_back(matrix[i][right]); 
         }
         right--;
-
-        // Bottom row
-        if (top <= bottom) {
-            for (int i = right; i >= left; i--) {
-                result.push_back(matrix[bottom][i]);
+        if(top<=bottom){
+            for(int i=right; i>=left; i--){
+                ans.push_back(matrix[bottom][i]); 
             }
-            bottom--;
+            bottom--; 
         }
 
-        // Left column
-        if (left <= right) {
-            for (int i = bottom; i >= top; i--) {
-                result.push_back(matrix[i][left]);
+        if(left<=right){
+            for(int i=bottom; i>=top; i--){
+                ans.push_back(matrix[i][left]); 
             }
             left++;
         }
 
         spiralHelper(matrix, top, bottom, left, right);
+
     }
 
     vector<int> spiralOrder(vector<vector<int>>& matrix) {
-        int n = matrix.size();
-        int m = matrix[0].size();
-        spiralHelper(matrix, 0, n - 1, 0, m - 1);
-        return result;
+        int n = matrix.size(); 
+        int m = matrix[0].size(); 
+
+        spiralHelper(matrix, 0, n-1, 0, m-1); 
+        return ans; 
     }
 };
