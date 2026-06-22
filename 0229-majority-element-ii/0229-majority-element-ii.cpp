5175a2752c0c8abce28dc928743b17ab72967ce0
Kanishk Srivastava <66626758+Kanishk-Srivastava@users.noreply.github.com>
2026-06-23 02:09:38 +0530

    Time: 0 ms (100%), Space: 23.7 MB (43.52%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.cpp
+++ 0229-majority-element-ii/0229-majority-element-ii.cpp
@@ -1,39 +1,33 @@
 class Solution {
 public:
     vector<int> majorityElement(vector<int>& nums) {
-        int candidate1=0, candidate2=0; 
+        int candidate1, candidate2; 
         int count1 = 0, count2 = 0; 
-
+        vector<int> result; 
         for(int i=0; i<nums.size(); i++){
             if(nums[i] == candidate1) count1++; 
             else if(nums[i] == candidate2) count2++; 
             else if(count1 == 0) {
                 candidate1 = nums[i]; 
-                count1++; 
+                count1++;
             } else if(count2 == 0){
                 candidate2 = nums[i]; 
-                count2++;
+                count2++; 
             } else {
                 count1--; 
                 count2--; 
             }
         }
 
-        int freq1 = 0, freq2 = 0; 
+        int freq1=0, freq2 = 0; 
 
         for(int i=0; i<nums.size(); i++){
             if(nums[i] == candidate1) freq1++; 
             if(nums[i] == candidate2) freq2++; 
         }
 
-        vector<int> ans; 
-        if(freq1 > floor(nums.size()/3)){
-            ans.push_back(candidate1); 
-        }
-        if(freq2 > floor(nums.size()/3)){
-            ans.push_back(candidate2); 
-        }
-
-        return ans; 
+        if(freq1 > floor(nums.size()/3)) result.push_back(candidate1); 
+        if(freq2 > floor(nums.size()/3)) result.push_back(candidate2);
+        return result;  
     }
 };
