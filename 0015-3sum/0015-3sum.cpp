754ee32fc956e9ff3de33f1e517009fe7e93417c
Kanishk Srivastava <66626758+Kanishk-Srivastava@users.noreply.github.com>
2026-06-24 00:24:09 +0530

    Time: 55 ms (28.62%), Space: 35.9 MB (11.55%) - LeetHub

--- 0015-3sum/0015-3sum.cpp
+++ 0015-3sum/0015-3sum.cpp
@@ -1,33 +1,40 @@
 class Solution {
 public:
-    vector<vector<int>> twoSum(int target, vector<int>& nums, int j, int k){
-        vector<vector<int>> result; 
-        while(j < k){
-            if(nums[j] + nums[k] > target) k--; 
-            else if(nums[j] + nums[k] < target) j++; 
+    vector<vector<int>> twoSum(vector<int>& nums, int target, int j, int k) {
+        vector<vector<int>> triplet;
+        while (j < k) {
+            if (nums[j] + nums[k] > target)
+                k--;
+            else if (nums[j] + nums[k] < target)
+                j++;
             else {
-                result.push_back({-target, nums[j], nums[k]}); 
-                j++; 
-                k--; 
-                while(j < k && nums[j] == nums[j-1]) j++; 
-                while(j < k && nums[k] == nums[k+1]) k--; 
+                int val = -target;
+                triplet.push_back({val, nums[j], nums[k]});
+                j++;
+                k--;
+                while (j < k && nums[j] == nums[j - 1])
+                    j++;
+                while (j < k && nums[k] == nums[k + 1])
+                    k--;
             }
         }
-        return result; 
+        return triplet;
     }
-
     vector<vector<int>> threeSum(vector<int>& nums) {
-        vector<vector<int>> result; 
         if(nums.size() < 3) return {}; 
+        vector<vector<int>> result;
 
-        sort(nums.begin(), nums.end()); 
+        sort(nums.begin(), nums.end());
 
-        for(int i=0; i<nums.size(); i++){
-            if(i > 0 && nums[i] == nums[i-1]) continue; 
-            int target = -nums[i]; 
-            vector<vector<int>>pairs = twoSum(target, nums, i+1, nums.size() - 1); 
-            result.insert(result.end(), pairs.begin(), pairs.end()); 
+        for (int i = 0; i < nums.size(); i++) {
+            if (i > 0 && nums[i] == nums[i - 1])
+                continue;
+            int target = -nums[i];
+
+            vector<vector<int>> triplet =
+                twoSum(nums, target, i + 1, nums.size() - 1);
+            result.insert(result.end(), triplet.begin(), triplet.end());
         }
-        return result; 
+        return result;
     }
 };
