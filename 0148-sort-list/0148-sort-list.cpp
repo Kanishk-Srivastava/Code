a0dea90cab09b710c281494f4de63e80ac37e08f
Kanishk Srivastava <66626758+Kanishk-Srivastava@users.noreply.github.com>
2026-06-29 13:01:49 +0530

    Time: 8 ms (90.99%), Space: 57 MB (89.41%) - LeetHub

--- 0148-sort-list/0148-sort-list.cpp
+++ 0148-sort-list/0148-sort-list.cpp
@@ -10,37 +10,15 @@
  */
 class Solution {
 public:
-    ListNode* sortList(ListNode* head) {
-        if(!head || !head->next){
-            return head; 
-        }
 
-        ListNode* prev = nullptr; 
-        ListNode* slow = head; 
-        ListNode* fast = head; 
-
-        while(fast && fast->next){
-            prev = slow; 
-            slow = slow->next; 
-            fast = fast->next->next; 
-        }
-
-        prev->next = nullptr; 
-
-        ListNode* l1 = sortList(head);
-        ListNode* l2 = sortList(slow); 
-
-        return merge(l1,l2);  
-    }
-
-    ListNode* merge(ListNode* l1, ListNode* l2){
+    ListNode* merge(ListNode* l1, ListNode*l2){
         ListNode dummy(0); 
         ListNode* tail = &dummy; 
 
         while(l1 && l2){
-            if(l1->val < l2->val){
+            if(l1->val < l2-> val){
                 tail->next = l1; 
-                l1 = l1->next; 
+                l1=l1->next; 
             } else {
                 tail->next = l2; 
                 l2 = l2->next; 
@@ -48,6 +26,31 @@ public:
             tail = tail->next; 
         }
         tail->next = l1 ? l1 : l2; 
-        return dummy.next; 
+        return dummy.next;
     }
+
+    ListNode* sortList(ListNode* head) {
+        if(!head || !head->next){
+            return head; 
+        }
+
+        ListNode* slow = head; 
+        ListNode* fast = head; 
+        ListNode* prev = head; 
+
+        while(fast && fast->next){
+            prev = slow; 
+            slow = slow -> next; 
+            fast = fast ->next -> next; 
+        }
+
+        prev->next = nullptr; 
+
+        ListNode* l1 = sortList(head); 
+        ListNode* l2 = sortList(slow); 
+
+        return merge(l1, l2); 
+    }
+
+
 };
