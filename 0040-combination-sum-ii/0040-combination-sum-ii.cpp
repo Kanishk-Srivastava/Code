1c0476879b0e0c6d579548ff7133089e9cb82788
Kanishk Srivastava <66626758+Kanishk-Srivastava@users.noreply.github.com>
2026-07-02 02:28:31 +0530

    Time: 0 ms (100%), Space: 14.1 MB (38%) - LeetHub

--- 0040-combination-sum-ii/0040-combination-sum-ii.cpp
+++ 0040-combination-sum-ii/0040-combination-sum-ii.cpp
@@ -1,27 +1,30 @@
 class Solution {
 public:
-    vector<vector<int>> combinationSum2(vector<int>& candidates, int target) {
-        sort(candidates.begin(), candidates.end());
-        vector<int> path;
-        vector<vector<int>> ans;
-        dfs(0, target, candidates, path, ans);
-        return ans;
+
+    void solve(int index, vector<int>& candidates,vector<int>& temp,vector<vector<int>>& ans, int target){
+ 
+            if(target == 0){
+                ans.push_back(temp); 
+                return; 
+            }
+
+        for(int i=index; i<candidates.size(); i++){
+            if(i > index && candidates[i] == candidates[i-1]){
+                continue; 
+            }
+            if(candidates[i] > target) break; 
+            temp.push_back(candidates[i]);
+            solve(i+1, candidates, temp, ans, target - candidates[i]);
+            temp.pop_back(); 
+        }
     }
 
-    void dfs(int start, int remain, vector<int>& candidates, vector<int>& path,
-             vector<vector<int>>& ans) {
-        if (remain == 0) {
-            ans.push_back(path);
-            return;
-        }
+    vector<vector<int>> combinationSum2(vector<int>& candidates, int target) {
+        sort(candidates.begin(), candidates.end());
+        vector<vector<int>> ans; 
+        vector<int> temp; 
 
-        for (int i = start; i < candidates.size(); i++) {
-            if(i > start && candidates[i-1] == candidates[i]) continue; 
-            int pick = candidates[i];
-            if(pick > remain) return; 
-            path.push_back(pick);
-            dfs(i + 1, remain - pick, candidates, path, ans);
-            path.pop_back();
-        }
+        solve(0, candidates, temp, ans, target); 
+        return ans; 
     }
 };
