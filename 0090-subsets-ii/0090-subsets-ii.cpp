943a75ed142f4c3030d3aabb2e9eaeb21f856c91
Kanishk Srivastava <66626758+Kanishk-Srivastava@users.noreply.github.com>
2026-07-02 18:41:33 +0530

    Time: 0 ms (100%), Space: 10.3 MB (97.26%) - LeetHub

--- 0090-subsets-ii/0090-subsets-ii.cpp
+++ 0090-subsets-ii/0090-subsets-ii.cpp
@@ -1,23 +1,23 @@
 class Solution {
 public:
-    vector<vector<int>> subsetsWithDup(vector<int>& nums) {
-        sort(nums.begin(), nums.end()); 
-        vector<int> path;
-        vector<vector<int>> ans;
-        dfs(0, nums, path, ans);
-        return ans;
+
+    void solve(int index, vector<int>& nums, vector<int>& temp, vector<vector<int>>& ans){
+        ans.push_back(temp); 
+
+        for(int i=index; i<nums.size(); i++){
+            if(i > index && nums[i] == nums[i-1]) continue; 
+
+            temp.push_back(nums[i]); 
+            solve(i+1, nums, temp, ans); 
+            temp.pop_back(); 
+        }
     }
 
-    void dfs(int start, vector<int>& nums, vector<int>& path,
-             vector<vector<int>>& ans) {
-        ans.push_back(path);
-
-        for (int i = start; i < nums.size(); i++) {
-            if (i > start && nums[i] == nums[i - 1])
-                continue;
-            path.push_back(nums[i]);
-            dfs(i + 1, nums, path, ans);
-            path.pop_back();
-        }
+    vector<vector<int>> subsetsWithDup(vector<int>& nums) {
+        vector<vector<int>> ans; 
+        vector<int> temp; 
+        sort(nums.begin(), nums.end()); 
+        solve(0, nums, temp, ans); 
+        return ans; 
     }
 };
