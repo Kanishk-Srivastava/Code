575737a9818ff306dc0e0c65240db01691a47276
Kanishk Srivastava <66626758+Kanishk-Srivastava@users.noreply.github.com>
2026-07-07 00:26:37 +0530

    Time: 0 ms (100%), Space: 10 MB (6.81%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.cpp
+++ 0020-valid-parentheses/0020-valid-parentheses.cpp
@@ -1,24 +1,26 @@
 class Solution {
 public:
     bool isValid(string s) {
-        stack<char> st;
-        bool valid = false;
-        for (int i = 0; i < s.size(); i++) {
-            if (s[i] == '(' || s[i] == '[' || s[i] == '{') {
-                st.push(s[i]);
+        stack<int> st; 
+        bool valid = false; 
+
+        for(int i=0; i<s.size(); i++){
+            if(s[i] == '(' || s[i] == '[' || s[i] == '{'){
+                st.push(s[i]); 
             }
-            if (s[i] == ')' || s[i] == ']' || s[i] == '}') {
-                if (st.empty()) {
-                    return false;
-                }
-                char ch = st.top();
-                st.pop();
-                if (ch == '(' && s[i] != ')' || ch == '[' && s[i] != ']' ||
-                    ch == '{' && s[i] != '}') {
-                    return false;
+
+            if(s[i] == ')' || s[i] == ']' || s[i] == '}'){
+                if(st.empty()) return false; 
+
+                char ch = st.top(); 
+                st.pop(); 
+
+                if((ch == '(' && s[i] != ')') || (ch == '[' && s[i] != ']') || (ch == '{' && s[i] != '}')){
+                    return false; 
                 }
             }
         }
+
         return st.empty(); 
     }
 };
